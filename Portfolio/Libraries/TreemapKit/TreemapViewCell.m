e582cd2cb27dbc73fa912bb2569f7feb64e7d5d3
Ausiàs <espaming@gmail.com>
2017-06-11 15:45:02 +0200

    - Fixes bug where labels where not being resized and laid out properly inside small treemap view cells. - Cleans up code to remove some hardcoded variables.

--- Portfolio/Libraries/TreemapKit/TreemapViewCell.m
+++ Portfolio/Libraries/TreemapKit/TreemapViewCell.m
@@ -9,6 +9,12 @@
 @synthesize index;
 @synthesize delegate;
 
+// Height of the labels
+int labelFrameHeight = 20;
+
+// Horizontal margin on right and left of the label
+int hSafeMargin = 2;
+
 #pragma mark -
 
 - (id)initWithFrame:(CGRect)frame {
@@ -16,9 +22,9 @@
         self.layer.borderWidth = 1.0;
         self.layer.borderColor = [[UIColor whiteColor] CGColor];
 
-        self.textLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, frame.size.width - 4, 20)];
-        textLabel.font = [UIFont boldSystemFontOfSize:20];
-        textLabel.baselineAdjustment = UIBaselineAdjustmentAlignCenters;
+        self.textLabel = [[UILabel alloc] initWithFrame:CGRectMake(hSafeMargin, 0, frame.size.width - hSafeMargin * 2, labelFrameHeight)];
+        textLabel.font = [UIFont systemFontOfSize:18];
+        textLabel.baselineAdjustment = UIBaselineAdjustmentNone;
         textLabel.textAlignment = NSTextAlignmentCenter;
         textLabel.textColor = [UIColor whiteColor];
         textLabel.backgroundColor = [UIColor clearColor];
@@ -26,8 +32,8 @@
         textLabel.adjustsFontSizeToFitWidth = YES;
         [self addSubview:textLabel];
 
-        self.valueLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, frame.size.width - 4, 20)];
-        valueLabel.font = [UIFont boldSystemFontOfSize:20];
+        self.valueLabel = [[UILabel alloc] initWithFrame:CGRectMake(hSafeMargin, 0, frame.size.width - hSafeMargin * 2, labelFrameHeight)];
+        valueLabel.font = [UIFont systemFontOfSize:12];
         valueLabel.baselineAdjustment = UIBaselineAdjustmentAlignCenters;
         valueLabel.textAlignment = NSTextAlignmentCenter;
         valueLabel.textColor = [UIColor whiteColor];
@@ -41,9 +47,17 @@
 
 - (void)layoutSubviews {
     [super layoutSubviews];
-
-    textLabel.frame = CGRectMake(0, self.frame.size.height / 2 - 10, self.frame.size.width, 20);
-    valueLabel.frame = CGRectMake(0, self.frame.size.height / 2 + 10, self.frame.size.width, 20);
+    
+    if (textLabel.frame.size.height > self.frame.size.height) {
+        // Text label too big to fit in the treemap view cell. We remove labels
+        [textLabel removeFromSuperview];
+        [valueLabel removeFromSuperview];
+    }
+    else {
+        // Adjust label to the size of the text, and keep centered
+        textLabel.frame = CGRectMake(hSafeMargin, self.frame.size.height / 3 - (labelFrameHeight / 2), self.frame.size.width - 4, textLabel.frame.size.height);
+        valueLabel.frame = CGRectMake(hSafeMargin, self.frame.size.height / 3 + (labelFrameHeight / 2), self.frame.size.width - 4, valueLabel.frame.size.height);
+    }
 }
 
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
