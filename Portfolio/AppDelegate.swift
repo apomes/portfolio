fa0ffc87b7e019533965cc561f1f65705dc25420
Ausiàs <espaming@gmail.com>
2017-08-30 20:19:49 +0200

    - Removes initial old views and view controllers which are no longer in use. - Adds initial implementation of a lock screen with fingerprint authentication unlock to keep data private.

--- Portfolio/AppDelegate.swift
+++ Portfolio/AppDelegate.swift
@@ -30,27 +30,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let myStoryboard:UIStoryboard = portfolioRootViewController.storyboard!
         lockScreenViewController = (myStoryboard.instantiateViewController(withIdentifier: "LockScreenID") as! LockScreenViewController)
         
+        lockScreen()
+//        lockScreenViewController.requestLocalAuthentication()
+        
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-        
-        // Hide info for privacy by presenting another view controller on top
-        if (!lockScreenViewController.isLocked) {
-//            portfolioRootViewController.performSegue(
-//                withIdentifier: "SegueToLockScreen",
-//                sender: portfolioRootViewController)
-            portfolioRootViewController.present(lockScreenViewController, animated: false, completion: nil)
-            lockScreenViewController.isLocked = true
-        }
-        
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        // Hide info for privacy by presenting another view controller on top
+        lockScreen()
         
         // Save portfolio data!
         portfolioRootViewController.savePortfolioData()
@@ -72,9 +68,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
     
     
-    
-    
-
+    func lockScreen() {
+        if (!lockScreenViewController.isLocked) {
+                portfolioRootViewController.present(lockScreenViewController, animated: true, completion: nil)
+                lockScreenViewController.isLocked = true
+        }
+    }
 
 }
 
