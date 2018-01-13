4a182ef51084eaa723838bce2d97b244e7383cc1
Ausiàs <espaming@gmail.com>
2018-01-13 17:56:34 +0100

    - Adds sample file to show how to keep your API keys and secrets. - Adds comment in ApiKeys.swift file on how to create your own ApiKeys.plist file.

--- Portfolio/ApiKeys.swift
+++ Portfolio/ApiKeys.swift
@@ -13,6 +13,9 @@ import Foundation
 
 /** Extracts the API keys values from a plist file.  */
 func apiKeyForTicker(tickerType:TickerType) -> String {
+    // NOTE: You should include your API key and secret to access ticker data in
+    // a file named ApiKeys.plist. This file will be ignored by Git so you don't
+    // leak sensitive data. A sample file named ApiKeysSample.plist is provided.
     let filePath = Bundle.main.path(forResource: "ApiKeys", ofType:"plist")
     let plist = NSDictionary(contentsOfFile:filePath!)
     let tickerSecret: NSDictionary = (plist?.object(forKey: tickerType.rawValue))! as! NSDictionary
