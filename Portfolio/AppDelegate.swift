615893afb042caa470d5942c47fbba5f66ad2ad1
Ausiàs <espaming@gmail.com>
2019-11-01 07:36:09 +0100

    Move Touch/Face ID to Enter foreground callback. Nos authentication is necessary only when coming back from background. This avoid issues with the active/resign active cycle that is triggered by the face ID itself.

--- Portfolio/AppDelegate.swift
+++ Portfolio/AppDelegate.swift
@@ -50,15 +50,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-
+        portfolioRootViewController.lockScreenViewController?.requestLocalAuthentication()
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         
-        if (portfolioRootViewController.lockScreenViewController!.isLocked) {
-            portfolioRootViewController.lockScreenViewController?.requestLocalAuthentication()
-        }
+        unlockScreen()
+        
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
@@ -78,6 +77,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
     
+    
+    func unlockScreen() {
+        portfolioRootViewController.lockScreenViewController!
+        .dismiss(animated: true, completion: nil)
+        portfolioRootViewController.lockScreenViewController?.isLocked = false
+    }
     
     
     func getTopViewController() -> UIViewController {
