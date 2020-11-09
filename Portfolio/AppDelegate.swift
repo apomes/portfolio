fae3e29aabf809db9a1b64a3e891c39fa6314806
Ausiàs <espaming@gmail.com>
2020-11-09 01:51:33 +0100

    Bugfix. Fix nil reference with lock screen controller when launching the app.

--- Portfolio/AppDelegate.swift
+++ Portfolio/AppDelegate.swift
@@ -58,8 +58,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
         
-        if (portfolioRootViewController.lockScreenViewController!.authenticationCorrect) {
-            unlockScreen()
+        if (portfolioRootViewController.lockScreenViewController != nil) {
+            if (portfolioRootViewController.lockScreenViewController!.authenticationCorrect) {
+                        unlockScreen()
+                    }
         }
     }
 
